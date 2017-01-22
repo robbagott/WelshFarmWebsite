cee7bf3a528d1ea9b74eec356cc0baf1e49de833
Robert Bagott <robbagott@gmail.com>
2017-01-22 00:18:33 -0600

    -Changed horse entry layout at horses link. -Added filtering functionality to horse entries.

--- src/public/horses/controllers/horses.client.controller.js
+++ src/public/horses/controllers/horses.client.controller.js
@@ -5,16 +5,64 @@
 		.module('horses')
 			.controller('HorsesController', HorsesController);
 
-	HorsesController.$inject = ['$routeParams', 'HorseApi'];
+	HorsesController.$inject = ['$routeParams', '$q', 'HorseApi'];
 
-	function HorsesController($routeParams, HorseApi) {
+	function HorsesController($routeParams, $q, HorseApi) {
 		var self = this;
 
-		self.find = find;
+		// Fields
+		self.list = [];
+		self.activeList = [];
+		self.filter = "all";
 
-		function find() {
-			self.list = HorseApi.query(function () {
+		// Methods
+		self.find = find;
+		self.init = init;
+		self.refreshList = refreshList;
+
+		function init() {
+			find().then(function () {
+				self.refreshList();
 			});
 		}
+
+		// find() returns a promise
+		function find() {
+			return $q(function (resolve, reject) {
+				self.list = HorseApi.query(function () {
+					console.log(self.list);
+					// Sort horses into categories when they come in.
+					// Also tack on age.
+					angular.forEach(self.list, function(horse, i) {
+						horse.categories = [];
+						horse.categories.push('all');
+
+						if (horse.sex === 'male') {
+							if (horse.gelded === true) {
+								horse.categories.push('stallions');
+							} else {
+								horse.categories.push('geldings');
+							}
+						} else {
+							horse.categories.push('mares');
+						}
+					});
+					resolve(self.list);
+				}, function (error) {
+						reject('Query for horses was unsuccessful');
+				});
+			});
+		}
+
+		function refreshList() {
+			self.activeList = [];
+			angular.forEach(self.list, function(horse, i) {
+				if (horse.categories.includes(self.filter)) {
+					self.activeList.push(horse);
+				}
+			});
+		}
+
+		
 	}
 })();
