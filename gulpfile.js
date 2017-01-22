5d674b90ca056630d63635e40e553f5a6a2518ca
Robert Bagott <robbagott@gmail.com>
2017-01-22 17:56:54 -0600

    -Added scss tasks. -Added devel/dist tasks. -Added gulp-plumber for better gulp error handling

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,35 @@
+var del = require('del');
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
-var del = require('del');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 
-gulp.task('clean', function () {
+// Clean Tasks
+
+gulp.task('clean-dist', function () {
 	return del(['dist']);
 });
 
+gulp.task('clean-devel', function () {
+	return del(['devel']);
+});
+
+gulp.task('clean', ['clean-dist', 'clean-devel']);
+
+// Distribution Tasks
+
 gulp.task('dist-js', function () {
 	return gulp.src('src/**/*.js')
+		.pipe(plumber())
 		.pipe(uglify())
 		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('dist-css', function () {
-	return gulp.src('src/**/*.css')
+gulp.task('dist-scss', function () {
+	return gulp.src('src/**/*.scss')
+		.pipe(plumber())
+		.pipe(sass().on('error', sass.logError))
 		.pipe(cleanCSS({compatibility: 'ie8'}))
 		.pipe(gulp.dest('dist'));
 });
@@ -23,11 +38,41 @@ gulp.task('dist-other', function () {
 	return gulp.src([
 		'src/**/*.ejs',
 		'src/**/*.html',
-		'src/public/lib'])
+		'src/public/lib',
+		'src/**/media/**.*'])
+		.pipe(plumber())
 		.pipe(gulp.dest('dist'));
-
 });
 
-gulp.task('dist', ['dist-js', 'dist-css', 'dist-other']);
+// Development Tasks
 
-gulp.task('default', ['dist']);
+gulp.task('devel-js', function () {
+	return gulp.src('src/**/*.js')
+		.pipe(plumber())
+		.pipe(gulp.dest('devel'));
+});
+
+gulp.task('devel-scss', function () {
+	return gulp.src('src/**/*.scss')
+		.pipe(plumber())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('devel'));
+});
+
+gulp.task('devel-other', function () {
+	return gulp.src([
+		'src/**/*.ejs',
+		'src/**/*.html',
+		'src/public/lib',
+		'src/**/media/**.*'])
+		.pipe(plumber())
+		.pipe(gulp.dest('devel'));
+});
+
+// Task Shortcuts
+
+gulp.task('dist', ['dist-js', 'dist-scss', 'dist-other']);
+
+gulp.task('devel', ['devel-js', 'devel-scss', 'devel-other']);
+
+gulp.task('default', ['dist', 'devel']);
